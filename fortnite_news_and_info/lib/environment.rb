f7c57f581f5757940f065286014b148e976215b0
Charlie <chrliejp0311@gmail.com>
2020-03-10 10:43:02 -0500

    First working commit

--- fortnite_news_and_info/lib/environment.rb
+++ fortnite_news_and_info/lib/environment.rb
@@ -1,6 +1,11 @@
-require "fortnite_news_and_info/version"
+require_relative "./fortnite_news_and_info/version"
+require_relative './fortnite_news_and_info/fortnite_news_and_info.rb'
+require_relative './fortnite_news_and_info/api.rb'
+require_relative './fortnite_news_and_info/cli.rb'
 
 module FortniteNewsAndInfo
   class Error < StandardError; end
   # Your code goes here...
 end
+
+#require_all './lib'
---
+++ fortnite_news_and_info/lib/fortnite_news_and_info/cli.rb
@@ -0,0 +1,5 @@
+class FortniteNewsAndInfo::CLI
+    def call
+        puts "I almost got this!"
+    end
+end
